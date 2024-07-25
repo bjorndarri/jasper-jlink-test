87b5de45f51dba2ea8b79d8f7dd345828c4218f3
Björn Darri Sigurðsson <bjorndarri@gmail.com>
2024-07-25 20:01:09 +0000

    Reduce to absolute minimum for https://github.com/TIBCOSoftware/jasperreports/issues/463

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,4 @@
 module is.test.jasper {
-  requires java.sql;
   requires jasperreports;
-  requires jasperreports.pdf;
-  requires org.apache.commons.logging;
-
-  exports is.test.jasper;
+  requires jasperreports.fonts;
 }
